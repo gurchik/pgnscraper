d726dcd18050fac12a89a0b0b8473155e9426b8b
Derek Maciel <derekamaciel@gmail.com>
2013-02-16 23:22:30 -0500

    Organized things better

--- pgnscraper.py
+++ pgnscraper.py
@@ -23,7 +23,7 @@ PASSWORD = ""
 #
 # Example on Windows:
 #     USERNAME = "C:\\Users\\Me\\pgns"
-# Example on Mac/Linux/Users
+# Example on Mac/Linux/Unix
 #     USERNAME = "/home/me/pgns"
 # Bad example with trailing slash
 #    USERNAME = "/home/me/pgns/"
@@ -33,7 +33,7 @@ import urllib2
 from bs4 import BeautifulSoup
 import re
 from os.path import exists
-from os import mkdir
+from os import makedirs
 
 def main():
     # If credentials supplied:
@@ -42,9 +42,12 @@ def main():
     # For each game type
     #types = ['echess', 'live_bullet', 'live_blitz', 'live_standard']
     types = ['echess']
+    
+    saved = 0
+    
     for gametype in types:
         
-        html = get_html("http://www.chess.com/home/game_archive?show={0}&member={1}".format(gametype, USERNAME))
+        html = get_file("http://www.chess.com/home/game_archive?show={0}&member={1}".format(gametype, USERNAME))
         soup = BeautifulSoup(html)
         
         # If no games are found of this type, go to the next type
@@ -54,78 +57,89 @@ def main():
             
         else:
             row = 0
-            saved = 0
             
             # For every game listed:
             while True:
                 game = {}
             
-                # If the row does not exist, go to the next game type 
-                if soup.find("td", id=re.compile("c14_row{0}".format(row))) == None:
+                # If the row 's first column does not exist, the row must not exist
+                # Go to the next game type
+                if get_table_cell(soup, row, 1) == None:
                     break
                     
-                # Cells are in the format "c14_row{ROW}_{COL}"
-                # where {ROW} starts from 0 but {COL} starts from 1
+                info = get_game_info(soup, row)
                 
-                game['type'] = gametype
-                
-                # White player
-                cell = soup.find("td", id=re.compile("c14_row{0}_1".format(row)))
-                game['white'] = cell.find_all("a")[1].get_text() #Player's name is the second link in the cell
-        
-                # Black player
-                cell = soup.find("td", id=re.compile("c14_row{0}_2".format(row)))
-                game['black'] = cell.find_all("a")[1].get_text() #Player's name is the second link in the cell
-        
-                # Number of moves
-                cell = soup.find("td", id=re.compile("c14_row{0}_5".format(row)))
-                game['moves'] = cell.get_text()
-        
-                # Game ID
-                cell = soup.find("td", id=re.compile("c14_row{0}_7".format(row)))
-                link = cell.find("a")['href'] # the ID is part of the URL of the link in the cell
-                game['id'] = link.replace("/echess/game?id=", "") # /echess/ for corresp., /livechess/ otherwise
-                
-                # Date
-                cell = soup.find("td", id=re.compile("c14_row{0}_6".format(row)))
-                date_str = cell.get_text() # the date is in mm/dd/yy and must be converted to yyyy_mm_dd
-                year = "20" + date_str.split("/")[2]
-                month = "%02d" % int(date_str.split("/")[0]) # Add leading zero if needed
-                day = "%02d" % int(date_str.split("/")[1]) # Add leading zero if needed
-                game['date'] = "{0}_{1}_{2}".format(year, month, day)
-                
-                
-                pgnroot = PGNPATH + "/correspondence"
-                if not exists(pgnroot):
-                    mkdir(pgnroot)
+                # Figure out where the PGN will go
+                if gametype == 'echess':
+                    pgnroot = PGNPATH + "/correspondence"
+                else:
+                    pgnroot = PGNPATH + "/live"
                     
-                path = pgnroot + "/" + year
+                path = pgnroot + "/" + info['year'] + "/" + info['month'] 
                 if not exists(path):
-                    mkdir(path)
-                
-                path = path + "/" + month
-                if not exists(path):
-                    mkdir(path)
+                    makedirs(path)
                     
                 # If the pgn does not exist, download it
-                filename = "{0}_vs_{1}_{2}_{3}_{4}.pgn".format(game['white'], game['black'], year, month, day)
-                if not exists(path + "/" + filename):
+                filename = "{0}_vs_{1}_{2}_{3}_{4}.pgn".format(info['white'], info['black'], info['year'], info['month'], info['day'])
+                destination = path + "/" + filename
+                if not exists(destination):
                     print "Downloading {0}".format(filename)
-                    output = open(path + "/" + filename, "wb")
-                    output.write(get_html("http://www.chess.com/echess/download_pgn?id={0}".format(game['id'])))
-                    output.close()
+                    url = "http://www.chess.com/echess/download_pgn?id={0}".format(game['id'])
+                    save_file(url, destination)
                     saved += 1
-                    
                 
                 row += 1
                 
     print "Saved {0} files".format(saved)
                     
         
-        
-def get_html(url):
+def get_file(url):
     response = urllib2.urlopen(url)
     return response.read()
+    
+def save_file(url, path):
+    data = get_file(url)
+    output = open(path, "wb")
+    output.write(data)
+    output.close
+    
+def get_table_cell(soup, row, col):
+    # All cells in the table have an id of "c14_row{row number}_{col number}"
+    expr = "c14_row{0}_{1}".format(row, col)
+    tag = soup.find("td", id=re.compile(expr))
+    return tag
+    
+def get_game_info(soup, row):
+    # Cells are in the format "c14_row{ROW}_{COL}"
+    # where {ROW} starts from 0 but {COL} starts from 1
+    
+    info = {}
+                
+    # White player
+    cell = get_table_cell(soup, row, 1)
+    info['white'] = cell.find_all("a")[1].get_text() #Player's name is the second link in the cell
+        
+    # Black player
+    cell = get_table_cell(soup, row, 2)
+    info['black'] = cell.find_all("a")[1].get_text() #Player's name is the second link in the cell
+        
+    # Number of moves
+    cell = get_table_cell(soup, row, 5)
+    info['moves'] = cell.get_text()
+        
+    # Game ID
+    cell = get_table_cell(soup, row, 7)
+    link = cell.find("a")['href'] # the ID is part of the URL of the link in the cell
+    info['id'] = link.replace("/echess/game?id=", "") # /echess/ for corresp., /livechess/ otherwise
+                
+    # Date
+    cell = get_table_cell(soup, row, 6)
+    date_str = cell.get_text() # the date is in mm/dd/yy and must be converted to yyyy, mm, and dd
+    info['year'] = "20" + date_str.split("/")[2]
+    info['month'] = "%02d" % int(date_str.split("/")[0]) # Add leading zero if needed
+    info['day'] = "%02d" % int(date_str.split("/")[1]) # Add leading zero if needed
+    
+    return info
 
 if __name__ == '__main__':
     main()
